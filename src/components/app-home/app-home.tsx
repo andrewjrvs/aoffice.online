4e0ac5bbce666aba0b65d458e484e655a8109af8
Andrew Jarvis <jarvisa@newholland.goodville.com>
2020-06-19 21:57:54 -0400

    inital commit

---
+++ src/components/app-home/app-home.tsx
@@ -0,0 +1,25 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'app-home',
+  styleUrl: 'app-home.css',
+  shadow: true
+})
+export class AppHome {
+
+  render() {
+    return (
+      <div class='app-home'>
+        <p>
+         This is a test page.
+        </p>
+
+        {/* <stencil-route-link url='/profile/stencil'>
+          <button>
+            Profile page
+          </button>
+        </stencil-route-link> */}
+      </div>
+    );
+  }
+}
