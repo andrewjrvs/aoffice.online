8c64655032c82f4b0a55d3ae8ed7fa96335821e4
Andrew Jarvis <6360004+andrewjrvs@users.noreply.github.com>
2021-04-02 19:18:32 +0000

    I'm confused... :-p

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -1,7 +1,7 @@
 import { Component, h } from '@stencil/core';
 import { RestrictedRoute } from '../restricted-route';
 import store from '../../utils/auth-store';
-
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 @Component({
   tag: 'app-root',
   styleUrl: 'app-root.css',
@@ -12,6 +12,12 @@ export class AppRoot {
 
   constructor() {
     fetch('/api/user').then((d) => d.json()).then(d =>  store.state.user = d);
+    
+  }
+
+  private logoff(): void {
+    fetch('./auth/logout');
+    store.state.user = null;
   }
 
   render() {
@@ -19,15 +25,22 @@ export class AppRoot {
       <div>
         <header>
           <h1><stencil-route-link url="/" exact={true}>A Office Online</stencil-route-link></h1>
-          {store.state.isAuthenticated ? <p>Welcome: {store.state.user.name}</p> : <p><stencil-route-link url="/login" exact={true}>Login</stencil-route-link></p>}
+          {store.state.isAuthenticated ? 
+              <p>
+                Welcome: {store.state.user.name}
+                &nbsp;<fa-i icon={faSignOutAlt} tabIndex={0} title="sign off" aria-label="sign off" onClick={() => this.logoff()}></fa-i>
+              </p> : 
+              <p>
+                <stencil-route-link url="/login" exact={true}>Login</stencil-route-link> 
+              </p>}
         </header>
 
         <main>
           <stencil-router>
             <stencil-route-switch scrollTopOffset={0}>
-              <stencil-route url='/' component='app-home' exact={true} />
-              <stencil-route url='/login' component='app-login' exact={true} />
-              <RestrictedRoute url='/users' oa_role='user_admin' component='app-users-list' exact={true} />
+              <stencil-route url={['/', 'home']} component='app-home' exact={true} />
+              <stencil-route url='/login' component='app-login' />
+              <RestrictedRoute url='/users' oa_role='user_admin' component='app-users-list' />
             </stencil-route-switch>
           </stencil-router>
         </main>
